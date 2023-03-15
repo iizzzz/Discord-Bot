17e82e2464913c06674e6efaa019f5ca93a958b8
iizzzz <replicaset02@github.com>
2023-03-15 22:26:10 +0900

    Initial Commit

---
+++ src/main/java/com/discord/DiscordApplication.java
@@ -0,0 +1,13 @@
+package com.discord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscordApplication.class, args);
+    }
+
+}
